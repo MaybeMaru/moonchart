121be502393cd71718569fd53f5011a2469985bd
swordcube <49110074+swordcube@users.noreply.github.com>
2026-04-06 23:36:29 -0500

    Convert complex V-Slice camera focus events into Position Camera events

--- src/moonchart/formats/fnf/FNFVSlice.hx
+++ src/moonchart/formats/fnf/FNFVSlice.hx
@@ -391,6 +391,23 @@ class FNFVSlice extends BasicJsonFormat<FNFVSliceFormat, FNFVSliceMeta>
 	{
 		switch (event.e)
 		{
+			case FNFVSlice.VSLICE_FOCUS_EVENT:
+				if(!(event.v is Int) && (event.v.x != null || event.v.y != null || event.v.duration != null || event.v.ease != null || event.v.easeDir != null)) {
+    				final data:BasicFNFPositionCameraEvent = {
+        				x: event.v.x,
+        				y: event.v.y,
+
+        				ease: (event.v.ease ?? "CLASSIC") + (event.v.easeDir ?? ""),
+                        duration: event.v.duration ?? 4.0,
+
+                        isOffset: true
+    				}
+                    return {
+                        time: event.t,
+                        name: BasicFNFEvent.POSITION_CAMERA,
+                        data: data
+                    }
+				}
 			case FNFVSlice.VSLICE_PLAY_ANIMATION_EVENT:
 				final data:BasicFNFPlayAnimEvent = {
 					target: event.v.target,
